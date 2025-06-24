d407936cb182b8ba946688964168d88c1559a7ca
nem4tgf <143236099+nem4tgf@users.noreply.github.com>
2025-06-24 09:33:28 +0700

    a

--- src/main/java/org/example/projetc_backend/ProjetcBackendApplication.java
+++ src/main/java/org/example/projetc_backend/ProjetcBackendApplication.java
@@ -1,13 +1,12 @@
 package org.example.projetc_backend;
 
-import io.github.cdimascio.dotenv.Dotenv;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProjetcBackendApplication {
 	public static void main(String[] args) {
-
 		SpringApplication.run(ProjetcBackendApplication.class, args);
 	}
 }
